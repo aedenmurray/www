33a71f8c6625c18d9648d378e117075eb6ae36ae
aedenmurray <aeden@aedenmurray.io>
2022-12-06 22:46:08 -0700

    Update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import '@fontsource/fira-code';
+import '@fontsource/fira-code/400.css';
 import '@fontsource/source-sans-pro/400.css';
 import '@fontsource/source-sans-pro/700.css';
 import './index.css';
