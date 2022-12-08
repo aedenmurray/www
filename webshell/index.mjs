acc6a889238c4a089a41300ff46ae7f4bb58952f
aedenmurray <aeden@aedenmurray.io>
2022-12-07 21:19:38 -0700

    Update

--- webshell/index.mjs
+++ webshell/index.mjs
@@ -1,4 +1,5 @@
 import AWS from 'aws-sdk';
+import { spawn } from 'child_process';
 
 const { API_ID, API_STAGE, AWS_REGION } = process.env;
 const managementAPI = new AWS.ApiGatewayManagementApi({
@@ -15,14 +16,58 @@ export const handler = async (event) => {
         };
     }
 
-    const output = 'xyz';
+    const post = (data) =>
+        managementAPI
+            .postToConnection({
+                ConnectionId: connectionId,
+                Data: data,
+            })
+            .promise();
 
-    await managementAPI
-        .postToConnection({
-            ConnectionId: connectionId,
-            Data: output,
-        })
-        .promise();
+    console.log(`Received command: ${command}`);
+    const child = spawn(command, { shell: true });
+
+    const promises = [];
+    await new Promise((resolve) => {
+        child.stdout.on('data', (data) => {
+            promises.push(
+                post(
+                    JSON.stringify({
+                        pid: child.pid,
+                        data: data.toString(),
+                        type: 'stdout',
+                        done: false,
+                    }),
+                ),
+            );
+        });
+
+        child.stderr.on('data', (data) => {
+            promises.push(
+                post(
+                    JSON.stringify({
+                        pid: child.pid,
+                        data: data.toString(),
+                        type: 'stderr',
+                        done: false,
+                    }),
+                ),
+            );
+        });
+
+        child.on('close', async (code) => {
+            await Promise.all(promises);
+            await post(
+                JSON.stringify({
+                    pid: child.pid,
+                    done: true,
+                    code,
+                }),
+            );
+
+            resolve();
+        });
+    });
 
     return {
         statusCode: 200,
