a75db9dc9840cc23948a58dde81cb6d23c5c2c6f
aedenmurray <aeden@aedenmurray.io>
2022-12-07 21:36:36 -0700

    Update

--- terraform/apigw.tf
+++ terraform/apigw.tf
@@ -40,3 +40,19 @@ resource "aws_lambda_permission" "apigw" {
   function_name = aws_lambda_function.webshell.function_name
   source_arn    = "${aws_apigatewayv2_api.webshell.execution_arn}/*/*"
 }
+
+resource "aws_apigatewayv2_domain_name" "webshell" {
+  domain_name = "ws.${local.domain}"
+
+  domain_name_configuration {
+    certificate_arn = aws_acm_certificate_validation.www.certificate_arn
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
+}
+
+resource "aws_apigatewayv2_api_mapping" "webshell" {
+  api_id      = aws_apigatewayv2_api.webshell.id
+  domain_name = aws_apigatewayv2_domain_name.webshell.id
+  stage       = aws_apigatewayv2_stage.main.id
+}
--- terraform/r53.tf
+++ terraform/r53.tf
@@ -49,3 +49,29 @@ resource "aws_route53_record" "www_ipv6" {
     evaluate_target_health = false
   }
 }
+
+resource "aws_route53_record" "webshell_ipv4" {
+  zone_id = data.aws_route53_zone.main.id
+  name    = aws_apigatewayv2_domain_name.webshell.domain_name
+  type    = "A"
+
+  alias {
+    name    = aws_apigatewayv2_domain_name.webshell.domain_name_configuration[0].target_domain_name
+    zone_id = aws_apigatewayv2_domain_name.webshell.domain_name_configuration[0].hosted_zone_id
+
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "webshell_ipv6" {
+  zone_id = data.aws_route53_zone.main.id
+  name    = aws_apigatewayv2_domain_name.webshell.domain_name
+  type    = "AAAA"
+
+  alias {
+    name    = aws_apigatewayv2_domain_name.webshell.domain_name_configuration[0].target_domain_name
+    zone_id = aws_apigatewayv2_domain_name.webshell.domain_name_configuration[0].hosted_zone_id
+
+    evaluate_target_health = false
+  }
+}
