9de2dd162937af40b007b7acae755d81955f9475
aedenmurray <aeden@aedenmurray.io>
2022-12-07 17:07:48 -0700

    Update

--- src/components/Shell/index.jsx
+++ src/components/Shell/index.jsx
@@ -1,8 +1,57 @@
 import { useEffect, useRef } from 'react';
-import WebShell from './WebShell';
+import LocalEchoController from 'local-echo';
+import { Terminal } from 'xterm';
+
 import '@fontsource/jetbrains-mono';
 import 'xterm/css/xterm.css';
 import './style.css';
+
+const banner = `
+██╗    ██╗███████╗██████╗ ███████╗██╗  ██╗███████╗██╗     ██╗     
+██║    ██║██╔════╝██╔══██╗██╔════╝██║  ██║██╔════╝██║     ██║     
+██║ █╗ ██║█████╗  ██████╔╝███████╗███████║█████╗  ██║     ██║     
+██║███╗██║██╔══╝  ██╔══██╗╚════██║██╔══██║██╔══╝  ██║     ██║     
+╚███╔███╔╝███████╗██████╔╝███████║██║  ██║███████╗███████╗███████╗
+ ╚══╝╚══╝ ╚══════╝╚═════╝ ╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝
+ \r\n
+ \r\n
+`;
+
+class WebShell extends Terminal {
+    constructor() {
+        super({
+            fontFamily: '"JetBrains Mono", monospace',
+            fontSize: 12,
+            rows: 30,
+        });
+
+        this.localEcho = new LocalEchoController();
+        this.loadAddon(this.localEcho);
+
+        this.localEcho.println(banner);
+
+        (function readLines() {
+            this.localEcho.read('webshell~$ ').then((data) => {
+                this.processInput(data);
+                readLines.bind(this)();
+            });
+        }.bind(this)());
+    }
+
+    processInput = (data) => {
+        try {
+            // eslint-disable-next-line no-eval
+            const output = eval(data);
+            if (output !== undefined) {
+                this.localEcho.println(output);
+                return;
+            }
+        } catch (error) {
+            const { message } = error;
+            this.localEcho.println(message);
+        }
+    };
+}
 
 const Shell = () => {
     const webTerminalElement = useRef(null);
