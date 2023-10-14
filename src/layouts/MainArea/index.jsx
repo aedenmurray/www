e6b9120c21c10e0d8098c62b799a2f5a50bf225c
Aeden Murray <aeden@aedenmurray.dev>
2023-10-14 12:03:48 -0700

    rm

--- src/layouts/MainArea/index.jsx
+++ src/layouts/MainArea/index.jsx
@@ -1,11 +1,9 @@
 import Repos from '../../components/Repos';
-import Shell from '../../components/Shell';
 import './style.css';
 
 const MainArea = () => {
     return (
         <div id="main-area">
-            <Shell />
             <Repos />
         </div>
     );
