ea63ed0e0d37f0ff21c37a885bb9306971225fd8
aedenmurray <aeden@aedenmurray.dev>
2024-11-15 20:07:02 -0700

    update default fetcher

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider, CssBaseline, Container } from '@mui/material';
+import { SWRConfig } from 'swr';
 import { Route } from 'wouter';
 import Header from 'components/Header';
 import Home from 'pages/Home';
@@ -16,17 +17,20 @@ import '@fontsource/roboto-mono/400.css';
 import '@fontsource/roboto-mono/500.css';
 import '@fontsource/roboto-mono/700.css';
 
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline>
-      <Header />
-      <Container sx={{ py: 2 }}>
-        <Route path="/posts" nest><Posts /></Route>
-        <Route path="/"><Home /></Route>
-      </Container>
-    </CssBaseline>
-  </ThemeProvider>,
+  <SWRConfig value={{ fetcher }}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <Header />
+        <Container sx={{ py: 2 }}>
+          <Route path="/posts" nest><Posts /></Route>
+          <Route path="/"><Home /></Route>
+        </Container>
+      </CssBaseline>
+    </ThemeProvider>
+  </SWRConfig>,
 );
