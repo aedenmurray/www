f65c2e51763b1b21c98d5b8980c9ccdd46bcca2b
aedenmurray <aeden@aedenmurray.dev>
2024-11-15 18:52:57 -0700

    update

--- src/components/ui/Card/CardHeader.jsx
+++ src/components/ui/Card/CardHeader.jsx
@@ -1,10 +1,11 @@
 import { Stack, Typography } from '@mui/material';
+import { cloneElement } from 'react';
 
 export default function CardHeader({ title, icon }) {
   return (
     <Stack direction="row" justifyContent="space-between">
-      <Typography fontWeight="bold" variant="body1">{title}</Typography>
-      {icon}
+      <Typography fontWeight="bold" variant="body2">{title}</Typography>
+      {icon ? cloneElement(icon, { fontSize: 'small' }) : null}
     </Stack>
   );
 }
