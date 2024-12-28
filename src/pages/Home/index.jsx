0418fa2617533991ad874e40eefd64ff73ecdb37
aedenmurray <aeden@aedenmurray.dev>
2024-12-28 12:15:12 -0700

    rate limit check

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,6 +12,19 @@ export default function Home() {
   const { posts } = postsHook;
   const { repos } = reposHook;
 
+  const reposRateLimited = reposHook?.error?.headers
+    ?.get('x-ratelimit-remaining') === '0';
+
+  const reposRateLimitReset = Number(
+    reposHook?.error?.headers
+      ?.get('x-ratelimit-reset'),
+  );
+
+  const reposRateLimit = Number(
+    reposHook?.error?.headers
+      ?.get('x-ratelimit-limit'),
+  );
+
   return (
     <Masonry>
       {posts.map((post) => (
@@ -41,7 +54,18 @@ export default function Home() {
         />
       ))}
 
-      {reposHook.error && (
+      {reposHook.error && reposRateLimited && (
+        <ErrorCard
+          title="Fetching Repositories Failed"
+          message={`
+            This page uses the public GitHub API to fetch the repositories.
+            The API has a rate limit of ${reposRateLimit} requests per hour (per IP).
+            The rate limit will reset at ${new Date(reposRateLimitReset * 1000)}
+          `}
+        />
+      )}
+
+      {reposHook.error && !reposRateLimited && (
         <ErrorCard
           title="Fetching Repositories Failed"
           message={`
