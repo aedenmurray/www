2da4273cb548dc596c4466f5ea62f6acf3554c41
aedenmurray <aeden@aedenmurray.dev>
2024-12-28 16:08:43 -0700

    update

--- src/components/ui/Masonry.jsx
+++ src/components/ui/Masonry.jsx
@@ -7,7 +7,11 @@ export default function Masonry({ children }) {
   return (
     <ResponsiveMasonry
       columnsCountBreakPoints={{
-        [breakpoints.values.xs]: 4,
+        [breakpoints.values.xs]: 1,
+        [breakpoints.values.sm]: 2,
+        [breakpoints.values.md]: 3,
+        [breakpoints.values.lg]: 4,
+        [breakpoints.values.xl]: 5,
       }}
     >
       <RRMasonry
