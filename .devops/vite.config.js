31496d3bbe60361436f04e8903d1dc84fa85a36d
aedenmurray <aeden@aedenmurray.dev>
2025-06-29 11:26:18 -0600

    update

--- .devops/vite.config.js
+++ .devops/vite.config.js
@@ -1,12 +1,12 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
+import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
 import { imagetools } from 'vite-imagetools';
 import sitemap from './sitemap';
 
-const plugins = [preact(), jsconfigPaths(), imagetools(), sitemap];
+const plugins = [react(), jsconfigPaths(), imagetools(), sitemap];
 const server = { open: false, port: 8000, hmr: false };
 const esbuild = { legalComments: 'none' };
 
